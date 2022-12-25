8f495800529365efc15e3da28c4d9f6f65dda939
Alexander Karpinets <alexanderkarpinets@gmail.com>
2022-12-25 21:49:45 +0300

    Initial Commit

---
+++ CounterApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CounterApp
+//
+//  Created by Karpinets Alexander on 25.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
